1d188ec24ac9f025abe616fa37127e426ed11b30
Flo-Wo <florianwolf1710@t-online.de>
2023-01-05 16:09:28 +0100

    added a python binding, as well as a small test script

---
+++ bindings/python/crocoddyl/core/activations/log-barrier.cpp
@@ -0,0 +1,49 @@
+#include "crocoddyl/core/activations/log-barrier.hpp"
+
+#include "python/crocoddyl/core/activation-base.hpp"
+#include "python/crocoddyl/core/core.hpp"
+namespace crocoddyl {
+namespace python {
+
+void exposeActivationLogBarrier() {
+  boost::python::register_ptr_to_python<
+      boost::shared_ptr<ActivationModelLogBarrier> >();
+
+  bp::class_<ActivationModelLogBarrier, bp::bases<ActivationModelAbstract> >(
+      "ActivationModelQuad",
+      "Quadratic activation model.\n\n"
+      "A quadratic action describes a quadratic function that depends on the "
+      "residual, i.e.\n"
+      "0.5 *||r||^2.",
+      bp::init<Eigen::VectorXd, Eigen::VectorXd, bp::optional<double> >(
+          bp::args("self", "weights", "bound", "damping"),
+          "Initialize the activation model.\n\n"
+          ":param weights: componentwise weights to linearly scale the "
+          "residual vector"
+          ":param bound: componentwise bound for the vector"
+          ":param damping: Float to simulate a step function (use 1/t for "
+          "large t)"))
+      .def("calc", &ActivationModelLogBarrier::calc,
+           bp::args("self", "data", "r"),
+           "Compute the 0.5 * ||r||^2.\n\n"
+           ":param data: activation data\n"
+           ":param r: residual vector")
+      .def("calcDiff", &ActivationModelLogBarrier::calcDiff,
+           bp::args("self", "data", "r"),
+           "Compute the derivatives of a quadratic function.\n\n"
+           "Note that the Hessian is constant, so we don't write again this "
+           "value.\n"
+           "It assumes that calc has been run first.\n"
+           ":param data: activation data\n"
+           ":param r: residual vector \n")
+      .def("createData", &ActivationModelLogBarrier::createData,
+           bp::args("self"), "Create the quadratic activation data.\n\n")
+      .add_property("weights",
+                    bp::make_function(&ActivationModelLogBarrier::get_weights,
+                                      bp::return_internal_reference<>()),
+                    &ActivationModelLogBarrier::set_weights,
+                    "weights of the quadratic term");
+}
+
+}  // namespace python
+}  // namespace crocoddyl
---
+++ test_log_barrier.cpp
@@ -0,0 +1,39 @@
+#include <boost/shared_ptr.hpp>
+
+#include "crocoddyl/core/activations/log-barrier.hpp"
+
+void test_computation() {
+  std::size_t nr = 1;
+  Eigen::VectorXd weights_log_barrier = 1. * Eigen::VectorXd::Ones(nr);
+  Eigen::VectorXd bound_log_barrier = 1.1 * Eigen::VectorXd::Ones(nr);
+  double damping = 0.5;
+
+  const boost::shared_ptr<crocoddyl::ActivationModelAbstract>& model =
+      boost::make_shared<crocoddyl::ActivationModelLogBarrier>(
+          weights_log_barrier, bound_log_barrier, damping);
+  // create the corresponding data object
+  boost::shared_ptr<crocoddyl::ActivationDataAbstract> data =
+      model->createData();
+
+  // Generating random input vector
+  const Eigen::VectorXd& r = Eigen::VectorXd::Random(model->get_nr());
+
+  std::cout << "Random input vector for the residual:" << std::endl;
+  std::cout << r << std::endl;
+  // data->a_value = nan("");
+
+  // Getting the state dimension from calc() call
+  model->calc(data, r);
+  model->calcDiff(data, r);
+  std::cout << "Value of the cost function:" << std::endl;
+  std::cout << data->a_value << std::endl;
+  std::cout << "Gradient of the objective:" << std::endl;
+  std::cout << data->Ar << std::endl;
+  std::cout << "Hessian of the objective:" << std::endl;
+  std::cout << (data->Arr).diagonal() << std::endl;
+}
+
+int main(int argc, char** argv) {
+  test_computation();
+  return 0;
+}
